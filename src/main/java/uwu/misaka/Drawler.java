399f7a2730db65739bad785c039b1f78b6708eb1
Misaka <misakachan2545@gmail.com>
2021-07-04 23:13:53 +0300

    Removing useless methods

--- src/main/java/uwu/misaka/Drawler.java
+++ src/main/java/uwu/misaka/Drawler.java
@@ -3,11 +3,9 @@ package uwu.misaka;
 import arc.graphics.Pixmap;
 import arc.struct.Seq;
 import mindustry.content.Blocks;
-import mindustry.core.World;
 import mindustry.world.blocks.defense.turrets.Turret;
 import mindustry.world.blocks.distribution.Conveyor;
 import mindustry.world.blocks.production.Drill;
-import mindustry.world.blocks.production.Fracker;
 import mindustry.world.blocks.production.SolidPump;
 import mindustry.world.blocks.units.Reconstructor;
 
@@ -20,8 +18,9 @@ public class Drawler {
     public Seq<FakeTile> tiles = new Seq<>();
     public int x_size;
     public int y_size;
+    public int offset = 32;
+
     public BufferedImage image;
-    public World world;
     Pixmap pixmap;
 
     public Drawler(Seq<FakeTile> tt, int w, int h) {
@@ -29,8 +28,8 @@ public class Drawler {
         y_size = h;
         tt.each(t -> tiles.add(t));
         System.out.println(x_size + " " + y_size);
-        image = new BufferedImage(x_size * 32, y_size * 32, BufferedImage.TYPE_INT_ARGB);
-        pixmap = new Pixmap(x_size * 32, y_size * 32);
+        image = new BufferedImage(x_size * offset, y_size * offset, BufferedImage.TYPE_INT_ARGB);
+        pixmap = new Pixmap(x_size * offset, y_size * offset);
         drawFloor();
         drawOverlay();
         drawBlocks();
@@ -48,7 +47,7 @@ public class Drawler {
                 continue;
             }
             try {
-                image.getGraphics().drawImage(Service.getMyPic(t.floor.region.toString()), t.x * 32, (y_size * 32) - (t.y * 32), null);
+                image.getGraphics().drawImage(Service.getMyPic(t.floor.region.toString()), t.x * offset, (y_size * offset) - (t.y * offset), null);
             } catch (NullPointerException | IOException e) {
                 e.printStackTrace();
                 break;
@@ -62,7 +61,7 @@ public class Drawler {
                 continue;
             }
             try {
-                image.getGraphics().drawImage(Service.getMyPic(t.overlay.region.toString()), t.x * 32, (y_size * 32) - (t.y * 32), null);
+                image.getGraphics().drawImage(Service.getMyPic(t.overlay.region.toString()), t.x * offset, (y_size * offset) - (t.y * offset), null);
             } catch (NullPointerException | IOException e) {
                 e.printStackTrace();
                 break;
@@ -77,58 +76,58 @@ public class Drawler {
             }
             try {
                 int y_offset = 0;
+
                 if (t.wall.size == 2) {
-                    y_offset = 1 * 32;
+                    y_offset = 1 * offset;
                 }
                 if (t.wall.size == 3) {
-                    y_offset = 1 * 32;
+                    y_offset = 1 * offset;
                 }
                 if (t.wall.size == 4) {
-                    y_offset = 2 * 32;
+                    y_offset = 2 * offset;
                 }
                 if (t.wall.size == 5) {
-                    y_offset = 2 * 32;
+                    y_offset = 2 * offset;
                 }
+
                 int x_offset = 0;
-                if (t.wall.size == 2) {
-                    //x_offset = 1*32;
-                }
+
                 if (t.wall.size == 3) {
-                    x_offset = 1 * 32;
+                    x_offset = 1 * offset;
                 }
                 if (t.wall.size == 4) {
-                    x_offset = 1 * 32;
+                    x_offset = 1 * offset;
                 }
                 if (t.wall.size == 5) {
-                    x_offset = 2 * 32;
+                    x_offset = 2 * offset;
                 }
 
                 if (t.wall instanceof Turret) {
-                    image.getGraphics().drawImage(Service.getMyPic("block-" + t.wall.size), t.x * 32 - x_offset, (y_size * 32) - (t.y * 32) - y_offset, null);
+                    image.getGraphics().drawImage(Service.getMyPic("block-" + t.wall.size), t.x * offset - x_offset, (y_size * offset) - (t.y * offset) - y_offset, null);
                 }
 
-                image.getGraphics().drawImage(Service.getMyPic(t.wall.region.toString()), t.x * 32 - x_offset, (y_size * 32) - (t.y * 32) - y_offset, null);
+                image.getGraphics().drawImage(Service.getMyPic(t.wall.region.toString()), t.x * offset - x_offset, (y_size * offset) - (t.y * offset) - y_offset, null);
 
                 if ((t.wall instanceof Reconstructor)) {
-                    image.getGraphics().drawImage(Service.getMyPic(t.wall.region.toString() + "-top"), t.x * 32 - x_offset, (y_size * 32) - (t.y * 32) - y_offset, null);
-                } else if (t.wall instanceof Drill || t.wall instanceof SolidPump || t.wall instanceof Fracker) {
-                    image.getGraphics().drawImage(Service.getMyPic(t.wall.region.toString() + "-rotator"), t.x * 32 - x_offset, (y_size * 32) - (t.y * 32) - y_offset, null);
-                    image.getGraphics().drawImage(Service.getMyPic(t.wall.region.toString() + "-top"), t.x * 32 - x_offset, (y_size * 32) - (t.y * 32) - y_offset, null);
+                    image.getGraphics().drawImage(Service.getMyPic(t.wall.region.toString() + "-top"), t.x * offset - x_offset, (y_size * offset) - (t.y * offset) - y_offset, null);
+                } else if (t.wall instanceof Drill || t.wall instanceof SolidPump) {
+                    image.getGraphics().drawImage(Service.getMyPic(t.wall.region.toString() + "-rotator"), t.x * offset - x_offset, (y_size * offset) - (t.y * offset) - y_offset, null);
+                    image.getGraphics().drawImage(Service.getMyPic(t.wall.region.toString() + "-top"), t.x * offset - x_offset, (y_size * offset) - (t.y * offset) - y_offset, null);
                 } else if (t.wall == Blocks.cultivator) {
-                    image.getGraphics().drawImage(Service.getMyPic(t.wall.region.toString() + "-middle"), t.x * 32 - x_offset, (y_size * 32) - (t.y * 32) - y_offset, null);
-                    image.getGraphics().drawImage(Service.getMyPic(t.wall.region.toString() + "-top"), t.x * 32 - x_offset, (y_size * 32) - (t.y * 32) - y_offset, null);
+                    image.getGraphics().drawImage(Service.getMyPic(t.wall.region.toString() + "-middle"), t.x * offset - x_offset, (y_size * offset) - (t.y * offset) - y_offset, null);
+                    image.getGraphics().drawImage(Service.getMyPic(t.wall.region.toString() + "-top"), t.x * offset - x_offset, (y_size * offset) - (t.y * offset) - y_offset, null);
                 } else if (t.wall instanceof Conveyor) {
                     if (t.wall != Blocks.plastaniumConveyor) {
-                        image.getGraphics().drawImage(Service.getMyPic(t.wall.region.toString() + "-0-0"), t.x * 32 - x_offset, (y_size * 32) - (t.y * 32) - y_offset, null);
+                        image.getGraphics().drawImage(Service.getMyPic(t.wall.region.toString() + "-0-0"), t.x * offset - x_offset, (y_size * offset) - (t.y * offset) - y_offset, null);
                     } else {
-                        image.getGraphics().drawImage(Service.getMyPic(t.wall.region.toString()), t.x * 32 - x_offset, (y_size * 32) - (t.y * 32) - y_offset, null);
+                        image.getGraphics().drawImage(Service.getMyPic(t.wall.region.toString()), t.x * offset - x_offset, (y_size * offset) - (t.y * offset) - y_offset, null);
                     }
                 }
                 if ((t.wall.teamRegion != null || t.team != null) && t.wall.synthetic()) {
                     if (t.wall.teamRegion.toString().equals("error")) {
-                        image.getGraphics().drawImage(Service.getMyPic(t.wall.teamRegion.toString()), t.x * 32 - x_offset, (y_size * 32) - (t.y * 32) - y_offset + (t.wall.size - 1) * 32, null);
+                        image.getGraphics().drawImage(Service.getMyPic(t.wall.teamRegion.toString()), t.x * offset - x_offset, (y_size * offset) - (t.y * offset) - y_offset + (t.wall.size - 1) * offset, null);
                     } else {
-                        image.getGraphics().drawImage(Service.getMyPic(t.wall.teamRegion.toString()), t.x * 32 - x_offset, (y_size * 32) - (t.y * 32) - y_offset, null);
+                        image.getGraphics().drawImage(Service.getMyPic(t.wall.teamRegion.toString()), t.x * offset - x_offset, (y_size * offset) - (t.y * offset) - y_offset, null);
                     }
                 }
 
--- src/main/java/uwu/misaka/Parser.java
+++ src/main/java/uwu/misaka/Parser.java
@@ -2,10 +2,8 @@ package uwu.misaka;
 
 import arc.Core;
 import arc.files.Fi;
-import arc.graphics.Color;
 import arc.graphics.Texture;
-import arc.graphics.g2d.*;
-import arc.math.Mathf;
+import arc.graphics.g2d.TextureAtlas;
 import arc.struct.ObjectMap;
 import mindustry.Vars;
 import mindustry.core.ContentLoader;
@@ -14,15 +12,9 @@ import mindustry.core.Version;
 import mindustry.core.World;
 import mindustry.ctype.Content;
 import mindustry.ctype.ContentType;
-import mindustry.world.Block;
 import mindustry.world.Tile;
-import mindustry.world.blocks.environment.OreBlock;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -31,8 +23,6 @@ import static mindustry.Vars.world;
 
 public class Parser {
     public ObjectMap<String, BufferedImage> regions;
-    Graphics2D currentGraphics;
-    BufferedImage currentImage;
     public static ObjectMap<String, Fi> imageFiles = new ObjectMap<>();
 
     public Parser() {
@@ -71,40 +61,7 @@ public class Parser {
             texture = reg.page.texture;
         }}));
 
-        Lines.useLegacyLine = true;
         Core.atlas.setErrorRegion("error");
-        Draw.scl = 1f / 4f;
-        Core.batch = new SpriteBatch(0) {
-            @Override
-            protected void draw(TextureRegion region, float x, float y, float originX, float originY, float width, float height, float rotation) {
-                x += 4;
-                y += 4;
-
-                x *= 4;
-                y *= 4;
-                width *= 4;
-                height *= 4;
-
-                y = currentImage.getHeight() - (y + height / 2f) - height / 2f;
-
-                AffineTransform at = new AffineTransform();
-                at.translate(x, y);
-                at.rotate(-rotation * Mathf.degRad, originX * 4, originY * 4);
-
-                currentGraphics.setTransform(at);
-                BufferedImage image = regions.get(((TextureAtlas.AtlasRegion) region).name);
-                if (!color.equals(Color.white)) {
-                    image = tint(image, color);
-                }
-
-                currentGraphics.drawImage(image, 0, 0, (int) width, (int) height, null);
-            }
-
-            @Override
-            protected void draw(Texture texture, float[] spriteVertices, int offset, int count) {
-                //do nothing
-            }
-        };
 
         for (ContentType type : ContentType.values()) {
             for (Content content : Vars.content.getBy(type)) {
@@ -114,19 +71,6 @@ public class Parser {
                 } catch (Throwable ignored) {
                 }
             }
-        }
-
-        try {
-            BufferedImage image = ImageIO.read(new File("sprites/block_colors.png"));
-
-            for (Block block : Vars.content.blocks()) {
-                block.mapColor.argb8888(image.getRGB(block.id, 0));
-                if (block instanceof OreBlock) {
-                    block.mapColor.set(block.itemDrop.color);
-                }
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
 
         world = new World() {
@@ -147,17 +91,4 @@ public class Parser {
         }
     }
 
-    private BufferedImage tint(BufferedImage image, Color color) {
-        BufferedImage copy = new BufferedImage(image.getWidth(), image.getHeight(), image.getType());
-        Color tmp = new Color();
-        for (int x = 0; x < copy.getWidth(); x++) {
-            for (int y = 0; y < copy.getHeight(); y++) {
-                int argb = image.getRGB(x, y);
-                tmp.argb8888(argb);
-                tmp.mul(color);
-                copy.setRGB(x, y, tmp.argb8888());
-            }
-        }
-        return copy;
-    }
 }
